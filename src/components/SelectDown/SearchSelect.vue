ac21ae8062866e2496ccd9e402bca10664ba60f3
shuiping123 <15733247011@163.com>
2021-08-05 18:12:06 +0800

    页面修改

--- src/components/SelectDown/SearchSelect.vue
+++ src/components/SelectDown/SearchSelect.vue
@@ -1,129 +1,167 @@
 <template>
-  <div>
-    <div class="searchDiv">
-      <!--时间选择-->
-      <time-choose v-if="showdate" ref="datetime" :type="showdate" :value.sync="date"></time-choose>
-      <!--时间选择-->
-      <time-choose v-if="showtime" ref="datetime" type="timerange" :value.sync="date2"></time-choose>
-      <!--单位选择-->
-      <tree-select v-if="com=='multiple'" :data="data"
-                   ref="coms"
-                   @changeCheck="changeCheck('com')"
-                   :checkData.sync="coms"
-                   :check-strictly="true"
-                   placeholder="请选择单位"
-                   :checkedKeys="coms"></tree-select>
-      <select-down-normal v-if="com!=='multiple'" :data="data"
-                          ref="coms"
-                          :check-strictly="true"
-                          :multiple="false"
-                          @changeCheck="changeCheck('com')"
-                          :checkData.sync="coms"
-                          placeholder="请选择单位"
-                          :checkedKeys="coms"></select-down-normal>
-      <!--部门-->
-      <select-down-normal v-if="dep=='multiple'" :data="dataForDep"
-                          ref="depRef"
-                          :check-strictly="false"
-                          :multiple="this.dep=='multiple'"
-                          @changeCheck="changeCheck('dep')"
-                          :checkData.sync="deps"
-                          placeholder="请选择部门"
-                          :checkedKeys="deps"></select-down-normal>
-      <!--用户-->
-      <select-down-normal v-if="user" :data="data2"
-                          ref="users"
-                          :check-strictly="false"
-                          :multiple="this.user=='multiple'"
-                          :checkData.sync="users"
-                          placeholder="请选择用户"
-                          :checkedKeys="users"></select-down-normal>
-      <!--采集方式-->
-      <select-down-normal v-if="apptype" :data="data1"
-                          ref="apptypes"
-                          :check-strictly="false"
-                          :multiple="this.apptype=='multiple'"
-                          :checkData.sync="apptypes"
-                          @changeCheck="changeCheck('apptype')"
-                          placeholder="请选择软件采集方式"
-                          :checkedKeys="apptypes"></select-down-normal>
-      <!--软件-->
-      <select-down-normal v-if="app" :data="data3"
-                          ref="apps"
-                          :check-strictly="false"
-                          :multiple="this.app=='multiple'"
-                          @changeCheck="changeCheck('app')"
-                          :checkData.sync="apps"
-                          placeholder="请选择软件"
-                          :checkedKeys="apps"></select-down-normal>
-      <!--模块-->
-      <select-down-normal v-if="module" :data="data4"
-                          ref="modules"
-                          :check-strictly="false"
-                          :multiple="this.module=='multiple'"
-                          :checkData.sync="modules"
-                          placeholder="请选择模块"
-                          :checkedKeys="modules"></select-down-normal>
-      <!--输入关键字-->
-      <div class="searchItem">
-        <el-input
-          v-if="showkeywords"
-          placeholder="请输入关键字"
-          v-model="keywords">
-          <i slot="prefix" class="el-input__icon el-icon-search"></i>
-        </el-input>
-      </div>
-      <!--查询条件 - 安装软件信息查询-->
-      <select-down-normal v-if="showsearchterm" :data="data5"
-                          ref="searchTermRef"
-                          :check-strictly="false"
-                          :multiple="this.showsearchterm=='multiple'"
-                          :checkData.sync="searchTerms"
-                          placeholder="请选择查询条件"
-                          :checkedKeys="searchTerms"></select-down-normal>
-      <!--是否卸载 - 安装软件信息查询-->
-      <select-down-normal v-if="showisunstall" :data="data6"
-                          ref="isUnstallRef"
-                          :check-strictly="false"
-                          :multiple="this.showisunstall=='multiple'"
-                          :checkData.sync="isUnstall"
-                          placeholder="请选择是否卸载"
-                          :checkedKeys="isUnstall"></select-down-normal>
-      <!--输入用户名-->
-      <div class="searchItem">
-        <el-input
-          v-if="showusername"
-          placeholder="请输入用户名"
-          v-model="userName">
-          <i slot="prefix" class="el-input__icon el-icon-search"></i>
-        </el-input>
-      </div>
-      <!--输入设备名-->
-      <div class="searchItem">
-        <el-input
-          v-if="showdevname"
-          placeholder="输入设备名"
-          v-model="devName">
-          <i slot="prefix" class="el-input__icon el-icon-search"></i>
-        </el-input>
-      </div>
-      <!--超时时间范围-->
-      <select-down-normal v-if="showovertime" :data="data7"
-                          ref="overTimeRef"
-                          :check-strictly="false"
-                          :multiple="this.showovertime=='multiple'"
-                          :checkData.sync="overTimeType"
-                          placeholder="请选择超时时间范围"
-                          :checkedKeys="overTimeType"></select-down-normal>
-      <!--查询按钮-->
-      <div class="searchItem">
-        <el-button type="primary" icon="el-icon-search" @click="SearchAction">查询</el-button>
-      </div>
-      <div class="searchItem">
-        <el-button type="primary" icon="el-icon-s-order">导出EXCEL</el-button>
-      </div>
+  <div class="SDiv" style="width:100%;">
 
-    </div>
+    <el-collapse v-model="activeNames" style="width: 100%;" >
+      <el-collapse-item  style="width: 100%;" name="1">
+        <template slot="title">
+          <i style="margin-left: 20px" class="header-icon el-icon-search"></i><span style="margin-left: 5px">查询条件</span>
+        </template>
+        <div class="searchDiv">
+          <!--时间选择-->
+          <time-choose  v-if="showdate" ref="datetime" :type="showdate" :value.sync="date"></time-choose>
+          <!--时间选择-->
+          <time-choose  v-if="showtime" ref="datetime" type="timerange" :value.sync="date2"></time-choose>
+          <!--单位选择-->
+          <tree-select  v-if="com=='multiple'" :data="data"
+                        ref="coms"
+                        @changeCheck="changeCheck('com')"
+                        :checkData.sync="coms"
+                        :check-strictly="true"
+                        placeholder="请选择单位"
+                        :checkedKeys="coms"></tree-select>
+          <select-down-normal   v-if="com!=='multiple'" :data="data"
+                                ref="coms"
+                                :check-strictly="true"
+                                :multiple="false"
+                                @changeCheck="changeCheck('com')"
+                                :checkData.sync="coms"
+                                placeholder="请选择单位"
+                                :checkedKeys="coms"></select-down-normal>
+          <div class="searchItem" v-if="com" style="padding: 10px 5px;">
+            <el-switch
+              v-model="BaokouAllChildComId"
+              active-text="包含子单位"
+              active-value="1"
+              inactive-value="0"
+              inactive-text="不包含">
+            </el-switch>
+          </div>
+          <!--部门-->
+          <select-down-normal  v-if="dep=='multiple'" :data="dataForDep"
+                               ref="depRef"
+                               :check-strictly="false"
+                               :multiple="this.dep=='multiple'"
+                               @changeCheck="changeCheck('dep')"
+                               :checkData.sync="deps"
+                               placeholder="请选择部门"
+                               :checkedKeys="deps"></select-down-normal>
+          <!--用户-->
+          <select-down-normal  v-if="user" :data="data2"
+                               ref="users"
+                               :check-strictly="false"
+                               :multiple="this.user=='multiple'"
+                               :checkData.sync="users"
+                               placeholder="请选择用户"
+                               :checkedKeys="users"></select-down-normal>
+          <!--采集方式-->
+          <select-down-normal  v-if="apptype" :data="data1"
+                               ref="apptypes"
+                               :check-strictly="false"
+                               :multiple="this.apptype=='multiple'"
+                               :checkData.sync="apptypes"
+                               @changeCheck="changeCheck('apptype')"
+                               placeholder="请选择软件采集方式"
+                               :checkedKeys="apptypes"></select-down-normal>
+          <!--软件-->
+          <select-down-normal  v-if="app" :data="data3"
+                               ref="apps"
+                               :check-strictly="false"
+                               :multiple="this.app=='multiple'"
+                               @changeCheck="changeCheck('app')"
+                               :checkData.sync="apps"
+                               placeholder="请选择软件"
+                               :checkedKeys="apps"></select-down-normal>
+          <!--模块-->
+          <select-down-normal  v-if="module" :data="data4"
+                               ref="modules"
+                               :check-strictly="false"
+                               :multiple="this.module=='multiple'"
+                               :checkData.sync="modules"
+                               placeholder="请选择模块"
+                               :checkedKeys="modules"></select-down-normal>
+          <!--输入关键字-->
+          <div class="searchItem">
+            <el-input
+              v-if="showkeywords"
+              placeholder="请输入关键字"
+              v-model="keywords">
+              <i slot="prefix" class="el-input__icon el-icon-search"></i>
+            </el-input>
+          </div>
+          <!--查询条件 - 安装软件信息查询-->
+          <select-down-normal  v-if="showsearchterm" :data="data5"
+                               ref="searchTermRef"
+                               :check-strictly="false"
+                               :multiple="this.showsearchterm=='multiple'"
+                               :checkData.sync="searchTerms"
+                               placeholder="请选择查询条件"
+                               :checkedKeys="searchTerms"></select-down-normal>
+          <!--是否卸载 - 安装软件信息查询-->
+          <select-down-normal  v-if="showisunstall" :data="data6"
+                               ref="isUnstallRef"
+                               :check-strictly="false"
+                               :multiple="this.showisunstall=='multiple'"
+                               :checkData.sync="isUnstall"
+                               placeholder="请选择是否卸载"
+                               :checkedKeys="isUnstall"></select-down-normal>
+          <!--输入用户名-->
+          <div  class="searchItem">
+            <el-input
+              v-if="showusername"
+              placeholder="请输入用户名"
+              v-model="userName">
+              <i slot="prefix" class="el-input__icon el-icon-search"></i>
+            </el-input>
+          </div>
+          <!--输入设备名-->
+          <div  class="searchItem">
+            <el-input
+              v-if="showdevname"
+              placeholder="输入设备名"
+              v-model="devName">
+              <i slot="prefix" class="el-input__icon el-icon-search"></i>
+            </el-input>
+          </div>
+          <!--超时时间范围-->
+          <select-down-normal  v-if="showovertime" :data="data7"
+                               ref="overTimeRef"
+                               :check-strictly="false"
+                               :multiple="this.showovertime=='multiple'"
+                               :checkData.sync="overTimeType"
+                               placeholder="请选择超时时间范围"
+                               :checkedKeys="overTimeType"></select-down-normal>
+          <!--查询按钮-->
+          <div class="searchItem" >
+            <el-button type="primary" icon="el-icon-search" @click="SearchAction">查询</el-button>
+          </div>
+          <div class="searchItem" >
+            <el-button type="primary" icon="el-icon-s-order">导出EXCEL</el-button>
+          </div>
+
+
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+
+
+
+
+<!--    <div v-show="!isSmall">-->
+<!--      <el-button type="primary" size="mini" title="导出EXCEL" icon="el-icon-s-order">导出</el-button>-->
+<!--    </div>-->
+
+
+<!--    <el-backtop>-->
+        <transition name="el-zoom-in-bottom">
+          <el-button v-show="isSmall" class="fiexdBtn" type="primary" size="medium" title="刷新" @click="SearchAction" icon="el-icon-refresh" circle></el-button>
+        </transition>
+        <transition name="el-zoom-in-bottom">
+          <el-button v-show="isSmall" class="fiexdBtn1" type="primary" size="medium" title="回到顶部" icon="el-icon-arrow-up" @click="toTopFUn" circle></el-button>
+        </transition>
+<!--    </el-backtop>-->
+
+
+    <!--    <div style="width: 100%;height: 20px;text-align: center"></div>-->
     <!--    <p>{{date}}</p>-->
     <!--    <p>{{date2}}</p>-->
     <!--    <p>单位：{{coms}}</p>-->
@@ -139,19 +177,23 @@
   import TreeSelect from '@/components/SelectDown/comp/SelectDown'
   import SelectDownNormal from '@/components/SelectDown/comp/SelectDownNormal'
   import TimeChoose from '@/components/SelectDown/comp/TimeChoose'
+  // 导入节流函数
+  import throttle from 'throttle-debounce/throttle';
 
   const URL='/Ashx/DropSelData.ashx';
 
   export default {
     name: 'searchSelect',
     props: [
-      'showdate', 'showtime', 'com', 'user',
+      'showdate', 'showtime', 'com', 'baohan','user',
       'apptype', 'app', 'module', 'showkeywords',
       'showsearchterm', 'showisunstall', 'showusername', 'showdevname',
       'showovertime', 'dep'],
     components: { TreeSelect, SelectDownNormal, TimeChoose },
     data() {
       return {
+        isSmall:false,
+        activeNames: ['1'],
         // 下拉菜单
         data: [],// 单位数据 - 下拉菜单
         dataForDep: [],// 部门数据 - 下拉菜单
@@ -176,6 +218,7 @@
 
         // 选中的数据
         coms: [],// 选中的单位数据
+        BaokouAllChildComId:"1",
         deps: [],// 选中的部门数据
         users: [],// 选中的用户数据
         apptypes: [],// 选中的软件类型数据
@@ -506,6 +549,7 @@
       getSearchData() {
         return {
           coms: this.coms,
+          BaokouAllChildComId:this.BaokouAllChildComId,
           deps: this.deps,
           users: this.users,
           apptypes: this.apptypes,
@@ -524,12 +568,16 @@
       // 查询执行的函数
       SearchAction(){
         this.$emit('searchAction')
+      },
+      toTopFUn(){
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
       }
     },
     mounted() {
       // 默认加载
       if (this.com) {
         this.getCom('init')
+        this.BaokouAllChildComId=this.baohan;
       }
       // 被联动数据，默认不在此初始加载,要写在联动管理里
       if (this.dep && !this.com) {
@@ -556,13 +604,32 @@
       if (this.showovertime) {
         this.getOvertimeType('init')
       }
+
+
+
+      // 滚动显示
+      window.onscroll = ()=>throttle(300, scrollFun());
+      const _this=this;
+      function scrollFun() {
+        var scrollTop = document.body.scrollTop || (document.documentElement && document.documentElement.scrollTop);
+        if (scrollTop >=400) {
+          _this.isSmall=true;
+        } else{
+          _this.isSmall=false;
+        }
+      }
+
+
+
+
     }
   }
 </script>
 
 <style>
-  .searchDiv {
+  .SDiv .searchDiv {
     width: 100%;
+    background: #F2F6FC;
     display: -ms-flex;
     display: -o-flex;
     display: -moz-flex;
@@ -572,8 +639,26 @@
 
   }
 
-  .searchItem {
+  .SDiv .searchItem {
     padding: 2px 2.5px;
   }
+  .SDiv .fiexdBtn{
+    position: fixed;
+    bottom: 150px;
+    right: 10px;
+    z-index: 1000;
+  }
+
+  .SDiv .fiexdBtn1{
+    position: fixed;
+    bottom: 100px;
+    right: 10px;
+    z-index: 1000;
+  }
+
+  .SDiv .el-collapse-item__header{
+    /*text-indent: 20px;*/
+  }
+
 </style>
 
--- src/views/test2/Test2-6.vue
+++ src/views/test2/Test2-6.vue
@@ -1,39 +1,160 @@
 <template>
-  <div style="width:100%">
-    <div class="searchDiv">
-      <search-select ref="searchSelect" com="single" showdate="daterange" app="multiple" dep="multiple"
-                     @searchAction="searchLoad"
-      ></search-select>
+  <div style="width:100%;">
+    <div>
+      <search-select ref="searchSelect" com="single" showdate="daterange" app="single" dep="multiple"
+                     baohan="1" @searchAction="searchLoad"></search-select>
     </div>
-    <button @click="getSearchData()">获取查询数据</button>
-    <p>显示结果</p>
-    <p>{{searchInfo}}</p>
-    <el-row>
-      <el-col :span="12" v-if="AppBasisInfo">
-        <p class="form-inline">
-          <img style="display: inline-block;line-height: 2vw;height: 1.6vw;width:auto"
-               :src="AppBasisInfo.iconUrl" class="form-group col-xs-1">
-          <!--                        <span style="background: #62BB46;color: #ffffff;display: inline-block;font-size: 12px;padding: 2px 8px;border-radius: 2px;">{{AppBasisInfo.country}}</span>-->
-          <span class="form-group col-xs-8" style="font-size: 1.6vw;color: #3e3e3e;padding-left: 5px;display: inline-block;line-height: 1.6vw; overflow: hidden!important;
+    <!--    <button @click="getSearchData()">获取查询数据</button>-->
+    <!--    <p>显示结果</p>-->
+    <!--    <p>{{searchInfo}}</p>-->
+    <div class="BgBody">
+
+      <el-row>
+        <el-col :span="12">
+          <div class="cardBoxItem">
+            <el-card  shadow="hover">
+              <div slot="header" class="clearfix">
+                <el-form :inline="true" class="demo-form-inline">
+                  <el-form-item>
+                      <el-avatar :size="35" shape="square" fit="fill" :src="AppBasisInfo.iconUrl"></el-avatar>
+                  </el-form-item>
+                  <el-form-item><span class="infoCardTitle">{{AppBasisInfo.name}}</span></el-form-item>
+                </el-form>
+              </div>
+                <div class="infoCardBody">
+                  <el-form label-position="left" label-width="80px" class="demo-form-inline" size="mini">
+                    <el-form-item label="国家：">
+                      <el-form :inline="true" size="mini">
+                        <el-form-item>
+                          <el-avatar :size="25" style="background:none" shape="square" fit="fill">
+                            <img style="display: inline-block;height:18px" :src="AppBasisInfo.countryUrl"
+                                 :title="AppBasisInfo.country">
+                          </el-avatar>
+                        </el-form-item>
+                        <el-form-item>
+                          <span>{{AppBasisInfo.country}}</span>
+                        </el-form-item>
+                      </el-form>
+
+                    </el-form-item>
+                    <el-form-item label="厂商：">
+                      <span>{{AppBasisInfo.appCom}}</span>
+                    </el-form-item>
+                    <el-form-item label="子软件：">
+                      <span>{{AppBasisInfo.childApp.join(' | ')}}</span>
+                    </el-form-item>
+                    <el-form-item label="类别：">
+                      <span>{{AppBasisInfo.type}}</span>
+                    </el-form-item>
+                    <el-form-item label="描述：">
+                      <span>{{AppBasisInfo.decInfo}}</span>
+                    </el-form-item>
+                  </el-form>
+
+                </div>
+            </el-card>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <el-col :span="8" style="height: 50%;">
+            <div class="cardBoxItem">
+              <el-card shadow="hover"></el-card>
+            </div>
+          </el-col>
+          <el-col :span="8" style="height: 50%;">
+            <div class="cardBoxItem">
+              <el-card shadow="hover"></el-card>
+            </div>
+          </el-col>
+          <el-col :span="8" style="height: 50%;">
+            <div class="cardBoxItem">
+              <el-card shadow="hover"></el-card>
+            </div>
+          </el-col>
+          <el-col :span="8" style="height: 50%;">
+            <div class="cardBoxItem">
+              <el-card shadow="hover"></el-card>
+            </div>
+          </el-col>
+          <el-col :span="8" style="height: 50%;">
+            <div class="cardBoxItem">
+              <el-card shadow="hover"></el-card>
+            </div>
+          </el-col>
+          <el-col :span="8" style="height: 50%;">
+            <div class="cardBoxItem">
+              <el-card shadow="hover"></el-card>
+            </div>
+          </el-col>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <div class="cardBoxItem">
+            <el-card :body-style="{ padding: '0px'}" shadow="hover">
+              <div slot="header" class="clearfix">
+<!--                <span>卡片名称</span>-->
+                <el-form :inline="true" class="demo-form-inline">
+                  <el-form-item>
+                    <img style="width: 2em" :src="AppBasisInfo.iconUrl"/>
+                  </el-form-item>
+                  <el-form-item>{{AppBasisInfo.name}}</el-form-item>
+                </el-form>
+              </div>
+              <div>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+
+                <p class="form-inline">
+
+                  <!--                        <span style="background: #62BB46;color: #ffffff;display: inline-block;font-size: 12px;padding: 2px 8px;border-radius: 2px;">{{AppBasisInfo.country}}</span>-->
+                  <span class="form-group col-xs-8" style="font-size: 1.6vw;color: #3e3e3e;padding-left: 5px;display: inline-block;line-height: 1.6vw; overflow: hidden!important;
             text-overflow: ellipsis!important;
-            white-space: nowrap;">{{AppBasisInfo.name}}</span>
-          <!--                        <span class="form-group col-xs-3 blueColorFont" style="font-size: 1vw;padding-left: 5px;display: inline-block;line-height: 1.6vw; overflow: hidden;-->
-          <!--            text-overflow: ellipsis;-->
-          <!--            white-space: nowrap;" id="zhankaiId" @click="showDecInfo(AppBasisInfo.decInfo)">[ 软件描述 <b class="glyphicon glyphicon-triangle-bottom"></b> ]</span>-->
-        </p>
-        <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw">国家：
-          <!--                        <img style="display: inline-block;height: 1vw" :src="guoqi(AppBasisInfo.country)"-->
-          <img style="display: inline-block;height: 1vw" :src="AppBasisInfo.countryUrl"
-               :title="AppBasisInfo.country">
-          {{AppBasisInfo.country}}</p>
-        <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw">厂商：{{AppBasisInfo.appCom}}</p>
-        <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw">子软件：{{AppBasisInfo.childApp.join(' | ')}}</p>
-        <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw">类别：{{AppBasisInfo.type}}</p>
-        <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw;">描述： {{AppBasisInfo.decInfo}}</p>
-        <!--                    <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw;height: 20%;background: #ebf2fa;white-space: normal;text-indent: 30px;" id="decId">{{AppBasisInfo.decInfo}}-->
-      </el-col>
-    </el-row>
-    <div style="background: #dee8ed;width: 100%;height: 500px"></div>
+            white-space: nowrap;"></span>
+                  <!--                        <span class="form-group col-xs-3 blueColorFont" style="font-size: 1vw;padding-left: 5px;display: inline-block;line-height: 1.6vw; overflow: hidden;-->
+                  <!--            text-overflow: ellipsis;-->
+                  <!--            white-space: nowrap;" id="zhankaiId" @click="showDecInfo(AppBasisInfo.decInfo)">[ 软件描述 <b class="glyphicon glyphicon-triangle-bottom"></b> ]</span>-->
+                </p>
+                <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw">国家：
+                  <!--                        <img style="display: inline-block;height: 1vw" :src="guoqi(AppBasisInfo.country)"-->
+                  <img style="display: inline-block;height: 1vw" :src="AppBasisInfo.countryUrl"
+                       :title="AppBasisInfo.country">
+                  {{AppBasisInfo.country}}</p>
+                <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw">厂商：{{AppBasisInfo.appCom}}</p>
+                <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw">子软件：{{AppBasisInfo.childApp.join(' | ')}}</p>
+                <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw">类别：{{AppBasisInfo.type}}</p>
+                <p style="color: #a4a4a4;font-size: 1vw;line-height: 1.2vw;">描述： {{AppBasisInfo.decInfo}}</p>
+              </div>
+            </el-card>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <div class="cardBoxItem">
+            <el-card :body-style="{ padding: '0px'}" shadow="hover">
+              <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                   class="image">
+              <div style="padding: 14px;">
+                <span>好吃的汉堡</span>
+                <div class="bottom clearfix">
+                  <time class="time">{{ }}</time>
+                  <el-button type="text" class="button">操作按钮</el-button>
+                </div>
+              </div>
+            </el-card>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
+
   </div>
 </template>
 
@@ -46,7 +167,19 @@
     data() {
       return {
         searchInfo: '',
-        AppBasisInfo: null
+        /**
+         * 软件的基础信息
+         * */
+        AppBasisInfo: {
+          name: '',//软件名
+          childApp: [''],//子软件/模块的数组
+          type: '',//类别
+          country: '',//国家
+          appCom: '',//厂商
+          iconUrl: null,//软件图标路径
+          countryUrl: '',//国家图标路径
+          decInfo: '',//描述
+        },
       }
     },
     components: { SearchSelect },
@@ -71,15 +204,15 @@
         }).then(res => {
           if (res.returncode == 0) {
             this.AppBasisInfo = res.returndata
-          }else if (res.returncode==2){
+          } else if (res.returncode == 2) {
             this.$alert(res.returnmsg, '错误信息', {
               confirmButtonText: '重新登录',
               callback: action => {
-                this.$current.toLoginOut(this);
+                this.$current.toLoginOut(this)
               }
-            });
-          }else {
-            this.$message.error(res.returnmsg);
+            })
+          } else {
+            this.$message.error(res.returnmsg)
           }
         })
       }
@@ -89,8 +222,24 @@
 
 <style>
   .searchDiv {
-    background: #F2F6FC;
+    /*background: #F2F6FC;*/
     width: 100%;
+  }
+
+  /*名片表头*/
+  .infoCardTitle{
+    font: 	25px Extra large;
+  }
+
+
+  /*名片表身*/
+  .infoCardBody{
+    padding: 20px;
+    font:14px Base ;
+  }
+
+  .infoCardBody .el-form-item{
+    margin-bottom: 5px;
   }
 
 </style>
